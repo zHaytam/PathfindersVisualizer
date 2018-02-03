bae02486cf10c05dbae89f8cdf9e82d2e16ba4ac
zHaytam <haytam.zanid@gmail.com>
2018-02-03 19:12:31 +0000

    Imported FormsModule
    This will enable nouislider to update the ngModel without me doing a little "hack".

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NouisliderModule } from 'ng2-nouislider';
 import { AppComponent } from './app.component';
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
     ],
     imports: [
         BrowserModule,
+        FormsModule,
         NouisliderModule
     ],
     providers: [],
